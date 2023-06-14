a68dc33407b06902284258f9b7a01bfc6cddadac
Snow Yang <ytm199891@gmail.com>
2023-06-14 14:27:22 +0800

    feat: Add beautiful drag n drop

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -7,12 +7,19 @@ import { v4 as uuidv4 } from "uuid";
 import { Edit, StickyNote, Trash, XSquare } from "lucide-react";
 import toast, { Toaster } from "react-hot-toast";
 import { Textarea } from "./textarea";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "react-beautiful-dnd";
 
 type TodoItem = {
   id: string;
   task: string;
   completed: boolean;
   isEditing: boolean;
+  isHighlighted?: boolean;
   hasDescription?: boolean;
   descriptionContent?: string;
 };
@@ -27,7 +34,9 @@ type TodoAction =
   | {
       type: "EDIT_DESCRIPTION";
       payload: { id: string; description: string };
-    };
+    }
+  | { type: "HIGHLIGHT_TODO"; payload: string }
+  | { type: "REORDER_TODO"; payload: TodoItem[] };
 
 // const initialState: TodoItem[] = [];
 
@@ -179,12 +188,25 @@ const reducer = (state: TodoItem[], action: TodoAction) => {
         if (todo.id === action.payload.id) {
           return {
             ...todo,
-
             descriptionContent: action.payload.description,
           };
         }
         return todo;
       });
+
+    case "HIGHLIGHT_TODO":
+      return state.map((todo) => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            isHighlighted: !todo.isHighlighted,
+          };
+        }
+        return todo;
+      });
+
+    case "REORDER_TODO":
+      return action.payload;
 
     default:
       return state;
@@ -194,8 +216,8 @@ const reducer = (state: TodoItem[], action: TodoAction) => {
 const TodoList = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [text, setText] = useState("");
-  const [targetId, setTargetId] = useState("");
-  const listRef = useRef<HTMLUListElement>(null);
+
+  const listRef = useRef<HTMLUListElement | null>(null);
 
   const notifyDuplicate = () =>
     toast.error("This task already exists!\nSearch it!", {
@@ -221,7 +243,6 @@ const TodoList = () => {
         task: text,
         completed: false,
         isEditing: false,
-
         hasDescription: false,
         descriptionContent: "",
       },
@@ -230,13 +251,31 @@ const TodoList = () => {
     setText("");
   };
 
-  // const handleSearch = () => {
-  //   if (!text) return;
+  const handleSearchTodo = () => {
+    if (!text) return;
 
-  //   const target = state.find((todo) => todo.task === text);
+    const target = state.find((todo) => todo.task === text);
 
-  //   return <TodoItem todo={target} dispatch={dispatch} />;
-  // };
+    if (target) {
+      const targetElement = document.getElementById(target.id);
+      dispatch({ type: "HIGHLIGHT_TODO", payload: target.id });
+      targetElement?.scrollIntoView({ behavior: "smooth", block: "center" });
+      setTimeout(() => {
+        dispatch({ type: "HIGHLIGHT_TODO", payload: target.id });
+      }, 2000);
+    }
+  };
+
+  const onDragEndHandler = (result: DropResult) => {
+    const { destination, source } = result;
+    if (!destination) return;
+
+    const items = Array.from(state);
+    const [reorderedItem] = items.splice(source.index, 1);
+    items.splice(destination.index, 0, reorderedItem);
+
+    dispatch({ type: "REORDER_TODO", payload: items });
+  };
 
   return (
     <div className="flex relative justify-around flex-col  min-h-[100dvh] w-full max-w-[36rem] md:max-w-xl items-center gap-3">
@@ -244,16 +283,24 @@ const TodoList = () => {
         Todo List
       </h1>
 
-      {/* <div ref={listRef} className="w-full px-3 mb-32 "> */}
-      <ul
-        ref={listRef}
-        className="absolute w-full h-full overflow-y-auto divide-y-2 bottom-32"
-      >
-        <TodoItem todos={state} dispatch={dispatch} />
-      </ul>
-      {/* </div> */}
+      <DragDropContext onDragEnd={onDragEndHandler}>
+        <Droppable droppableId={"listId"}>
+          {(provided) => (
+            <ul
+              ref={(ref) => {
+                provided.innerRef(ref);
+                listRef.current = ref;
+              }}
+              {...provided.droppableProps}
+              className="absolute divide-y-2 w-full h-full px-5 pt-[12rem] overflow-y-auto  bottom-32"
+            >
+              <TodoItem todos={state} dispatch={dispatch} />
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
 
-      <div className="fixed bottom-0 flex flex-col items-center justify-center w-full h-32 gap-3 text-gray-300 bg-gray-900/80 backdrop-blur-xl">
+      <div className="fixed bottom-0 flex flex-col items-center justify-center w-full h-32 gap-3 text-gray-100 bg-gray-900/80 backdrop-blur-xl">
         <Input
           type="text"
           className="w-64 caret-gray-300 placeholder:text-gray-400"
@@ -267,7 +314,12 @@ const TodoList = () => {
           <Button className="w-[7.5rem] dark" onClick={() => handleAddTodo()}>
             Add
           </Button>
-          <Button className="w-[7.5rem] dark">Search</Button>
+          <Button
+            className="w-[7.5rem] dark"
+            onClick={() => handleSearchTodo()}
+          >
+            Search
+          </Button>
         </div>
       </div>
 
@@ -300,84 +352,97 @@ type TodoItemProps = {
 const TodoItem = ({ todos, dispatch }: TodoItemProps) => {
   return (
     <>
-      {todos.map((todo: TodoItem) => (
-        <li
+      {todos.map((todo: TodoItem, index) => (
+        <Draggable
           key={todo.id}
-          className={`${
-            todo.completed ? "text-gray-500 line-through" : "text-gray-900"
-          } flex justify-between py-2 items-center  w-full gap-x-2`}
+          draggableId={todo.id}
+          index={index}
+          isDragDisabled={todo.isEditing}
         >
-          <input
-            type="checkbox"
-            className="h-6 w-6 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
-            checked={todo.completed}
-            onChange={() =>
-              dispatch({ type: "ISFINISHED_TODO", payload: todo.id })
-            }
-          />
-          <div className="text-xl">
-            {todo.isEditing ? (
-              <Input
-                type="text"
-                value={todo.task}
-                className="text-xl"
-                onChange={(e) =>
-                  dispatch({
-                    type: "EDIT_TODO_CONTENT",
-                    payload: { id: todo.id, task: e.target.value },
-                  })
+          {(provided, snapshot) => (
+            <li
+              id={todo.id}
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              className={`${snapshot.isDragging ? "bg-gray-100" : ""} ${
+                todo.completed ? "text-gray-500 line-through" : "text-gray-900"
+              } ${
+                todo.isHighlighted ? "text-yellow-400" : ""
+              } flex justify-between py-2 items-center  w-full gap-x-2`}
+            >
+              <input
+                type="checkbox"
+                className="h-6 w-6 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+                checked={todo.completed}
+                onChange={() =>
+                  dispatch({ type: "ISFINISHED_TODO", payload: todo.id })
                 }
               />
-            ) : (
-              <p>{todo.task}</p>
-            )}
+              <div className="text-xl">
+                {todo.isEditing ? (
+                  <Input
+                    type="text"
+                    value={todo.task}
+                    className="text-xl"
+                    onChange={(e) =>
+                      dispatch({
+                        type: "EDIT_TODO_CONTENT",
+                        payload: { id: todo.id, task: e.target.value },
+                      })
+                    }
+                  />
+                ) : (
+                  <p>{todo.task}</p>
+                )}
 
-            {todo.hasDescription ? (
-              <Textarea
-                placeholder="Add description"
-                value={todo.descriptionContent}
-                className="h-8 text-gray-500"
-                onChange={(e) =>
-                  dispatch({
-                    type: "EDIT_DESCRIPTION",
-                    payload: { id: todo.id, description: e.target.value },
-                  })
-                }
-              />
-            ) : (
-              ""
-            )}
-          </div>
-
-          <div className="flex gap-2">
-            <Button
-              onClick={() =>
-                dispatch({ type: "ISEDIT_TODO", payload: todo.id })
-              }
-            >
-              <Edit size={20} />
-            </Button>
-            <Button
-              onClick={() =>
-                dispatch({ type: "TOGGLE_DESCRIPTION", payload: todo.id })
-              }
-            >
-              {todo.hasDescription ? (
-                <XSquare size={20} />
-              ) : (
-                <StickyNote size={20} />
-              )}
-            </Button>
-            <Button
-              className="text-sm text-red-500"
-              onClick={() =>
-                dispatch({ type: "DELETE_TODO", payload: todo.id })
-              }
-            >
-              <Trash size={20} />
-            </Button>
-          </div>
-        </li>
+                {todo.hasDescription ? (
+                  <Textarea
+                    placeholder="Add description"
+                    value={todo.descriptionContent}
+                    className="h-8 text-gray-500"
+                    onChange={(e) =>
+                      dispatch({
+                        type: "EDIT_DESCRIPTION",
+                        payload: { id: todo.id, description: e.target.value },
+                      })
+                    }
+                  />
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className="flex gap-2">
+                <Button
+                  onClick={() =>
+                    dispatch({ type: "ISEDIT_TODO", payload: todo.id })
+                  }
+                >
+                  <Edit size={20} />
+                </Button>
+                <Button
+                  onClick={() =>
+                    dispatch({ type: "TOGGLE_DESCRIPTION", payload: todo.id })
+                  }
+                >
+                  {todo.hasDescription ? (
+                    <XSquare size={20} />
+                  ) : (
+                    <StickyNote size={20} />
+                  )}
+                </Button>
+                <Button
+                  className="text-sm text-red-500"
+                  onClick={() =>
+                    dispatch({ type: "DELETE_TODO", payload: todo.id })
+                  }
+                >
+                  <Trash size={20} />
+                </Button>
+              </div>
+            </li>
+          )}
+        </Draggable>
       ))}
     </>
   );
